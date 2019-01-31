d5e377db6d384dcb18c8202a706dd0cdb2834f93
Ethan Mick <ethan@ethanmick.com>
2019-01-31 09:56:12 -0500

    Added tares page

---
+++ frontend/pages/tares.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Tares extends React.Component {
+  static async getInitialProps({ query }) {
+    return {}
+  }
+
+  public render() {
+    return <div>user tares</div>
+  }
+}
