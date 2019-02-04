63d01202f8c0f0bc654bb064202ea2ee95bfea6c
Ethan Mick <ethan@ethanmick.com>
2019-02-04 15:58:00 -0500

    Add seed file for DB

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, email, avatar_url, name) VALUES
+('em', 'ethan@muffinmethod.com', 'http://www.pngonly.com/wp-content/uploads/2017/06/Apple-Fruit-Transparent-PNG.png', 'Ethan Mick'),
+('ben', 'ben@muffinmethod.com', 'http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c17d.png', 'Ben');
