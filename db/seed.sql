3a68eefe3eee3775571307022064a021bdb64660
Ben Burwell <ben@benburwell.com>
2019-03-04 09:43:20 -0500

    Set password for bb dev user

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (username, email, password_hash, avatar_url, name) VALUES
 ('em', 'em@platezero.com', '$2a$10$xtLGz6w08W2cYtk8h24Dw.gFoykXm3or5ghJQCIGREpQFsXTbZVci', 'http://www.pngonly.com/wp-content/uploads/2017/06/Apple-Fruit-Transparent-PNG.png', 'Ethan Mick'),
-('ben', 'bb@platezero.com', NULL, 'http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c17d.png', 'Ben');
+('bb', 'bb@platezero.com', '$2a$15$eej36zpMJ88xtKGDnRYslOx4AhXbJ5BDV7CLunz3F0ICM7/xRaXqW', 'http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c17d.png', 'Ben');
