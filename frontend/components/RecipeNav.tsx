4e95b1e9b7e044cb39fd3226f108ebf6c2fbb5fd
Ben Burwell <ben@benburwell.com>
2019-03-06 11:38:58 -0500

    [frontend] Use RecipeNav component

---
+++ frontend/components/RecipeNav.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Recipe as RecipeModel } from '../models/recipe'
+const {
+  routes: { Link }
+} = require('../routes')
+
+interface BranchProps {
+  name: string
+  selected: boolean
+  route: string
+}
+
+const Branch = (props: BranchProps) => {
+  const color = props.selected ? 'btn-primary' : 'btn-secondary'
+  return (
+    <Link route={props.route}>
+      <a className={'btn btn-sm ' + color}>{props.name}</a>
+    </Link>
+  )
+}
+
+interface RecipeNavProps {
+  recipe: RecipeModel
+  selectedRecipeVersion?: number
+}
+
+export const RecipeNav = (props: RecipeNavProps) => {
+  const { recipe } = props
+  const { owner, branches } = recipe
+  return (
+    <div className="my-3">
+      <h1>
+        <Link route={`/${owner.username}/recipes`}>{owner.username}</Link>
+        <span className="text-secondary"> / </span>
+        <a>{recipe.title}</a>
+      </h1>
+      <div>
+        {branches.map(b => (
+          <Branch
+            key={b.recipe_version_id}
+            name={b.name}
+            route={`/${owner.username}/${recipe.slug}/versions/${
+              b.recipe_version_id
+            }`}
+            selected={b.recipe_version_id === props.selectedRecipeVersion}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
--- frontend/pages/recipe.tsx
+++ frontend/pages/recipe.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Layout } from '../components'
+import { Layout, RecipeNav } from '../components'
 import Head from 'next/head'
-import { Row, Col } from 'reactstrap'
 import { Recipe as RecipeModel } from '../models/recipe'
 import { getRecipe } from '../common/http'
-const {
-  routes: { Link }
-} = require('../routes')
 
 interface RecipeProps {
   recipe: RecipeModel
@@ -19,30 +15,12 @@ export default class Recipe extends React.Component<RecipeProps> {
 
   public render() {
     const { recipe } = this.props
-    const { owner } = recipe
     return (
       <Layout>
         <Head>
           <title>{recipe.title}</title>
         </Head>
-        <h1>
-          <Link route={`/${owner.username}/recipes`}>{owner.username}</Link> /{' '}
-          <a>{recipe.title}</a>
-        </h1>
-        <Row>
-          {recipe.branches.map(b => (
-            <Col key={b.recipeVersion.id}>
-              <Link
-                route={`/${owner.username}/${recipe.slug}/versions/${
-                  b.recipeVersion.id
-                }`}
-              >
-                <a>{b.name}</a>
-              </Link>
-            </Col>
-          ))}
-        </Row>
-        {this.props.children}
+        <RecipeNav recipe={recipe} />
       </Layout>
     )
   }
