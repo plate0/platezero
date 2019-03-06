33e5267285fde372ae53988362987238aafecfed
Ethan Mick <ethan@ethanmick.com>
2019-03-06 10:46:08 -0500

    Update my profile picture

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (username, email, password_hash, avatar_url, name) VALUES
-('em', 'em@platezero.com', '$2a$10$xtLGz6w08W2cYtk8h24Dw.gFoykXm3or5ghJQCIGREpQFsXTbZVci', 'http://www.pngonly.com/wp-content/uploads/2017/06/Apple-Fruit-Transparent-PNG.png', 'Ethan Mick'),
+('em', 'em@platezero.com', '$2a$10$xtLGz6w08W2cYtk8h24Dw.gFoykXm3or5ghJQCIGREpQFsXTbZVci', 'https://scontent-lga3-1.cdninstagram.com/vp/e004fffdc9b238943342326a7a7d3da0/5D17860A/t51.2885-15/e35/43049446_2250161478359217_6673761937676218532_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com', 'Ethan Mick'),
 ('bb', 'bb@platezero.com', '$2a$15$eej36zpMJ88xtKGDnRYslOx4AhXbJ5BDV7CLunz3F0ICM7/xRaXqW', 'http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c17d.png', 'Ben');
