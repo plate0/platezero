c41ee1926ae9357b0fdfba709f1e2af144c57ad2
Ben Burwell <ben@benburwell.com>
2019-03-11 11:37:51 -0400

    [frontend] Add "else" property to IfLoggedIn component
    This will allow us to render some default content in the case that the
    user is _not_ logged in. Simply pass an element as the else and you're
    good to go:
    
      <IfLoggedIn else={<p className="text-danger">Not logged in</p>}>
        <p className="text-success">Logged in!</p>
      </IfLoggedIn>

--- frontend/components/IfLoggedIn.tsx
+++ frontend/components/IfLoggedIn.tsx
@@ -4,25 +4,32 @@ import { PlateZeroContext } from '../pages/_app'
 
 interface IfLoggedInProps {
   username?: string
+  else?: React.Element
 }
 
 export class IfLoggedIn extends React.Component<IfLoggedInProps> {
   public static contextType = PlateZeroContext
 
+  constructor(props: IfLoggedInProps) {
+    super(props)
+    this.guarded = this.props.children
+    this.default = this.props.else || null
+  }
+
   public render() {
     // if a username is provided and matches the currently logged in user
     if (this.props.username) {
       return get(this.context, 'user.username') === this.props.username
-        ? this.props.children
-        : null
+        ? this.guarded
+        : this.default
     }
 
     // if a user is logged in
     if (this.context.user) {
-      return this.props.children
+      return this.guarded
     }
 
     // by default, render nothing
-    return null
+    return this.default
   }
 }
