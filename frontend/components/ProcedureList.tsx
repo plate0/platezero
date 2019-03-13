e8bd3fc405634870103670ff3fcb48e1842da8d8
Ben Burwell <github@benburwell.com>
2019-03-13 15:32:11 -0400

    Implement amount parsing (#14)

---
+++ frontend/components/ProcedureList.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Button, Col, Input, Row } from 'reactstrap'
+import * as _ from 'lodash'
+
+import { ProcedureListJSON, ProcedureLineJSON } from '../models'
+
+interface Props {
+  procedureList?: ProcedureListJSON
+  onChange?: (procedureList: ProcedureListJSON) => void
+}
+
+interface State {
+  name?: string
+  steps: ProcedureLineJSON[]
+}
+
+export class ProcedureList extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.notifyChange = this.notifyChange.bind(this)
+    this.replaceStep = this.replaceStep.bind(this)
+    this.state = props.procedureList
+      ? props.procedureList
+      : {
+          steps: [{ text: '' }]
+        }
+  }
+
+  public notifyChange() {
+    if (this.props.onChange) {
+      this.props.onChange(this.state)
+    }
+  }
+
+  public replaceStep(idx: number, text: string): void {
+    this.setState(
+      state => ({
+        steps: _.map(state.steps, (step, i) =>
+          Object.assign(step, i === idx ? { text } : undefined)
+        )
+      }),
+      this.notifyChange
+    )
+  }
+
+  public render() {
+    return (
+      <div>
+        {this.state.steps.map((s, key) => (
+          <Row key={key}>
+            <Col className="mb-3">
+              <Input
+                key={key}
+                type="textarea"
+                placeholder="Step by step instructions..."
+                value={s.text}
+                onChange={e => this.replaceStep(key, e.target.value)}
+              />
+            </Col>
+          </Row>
+        ))}
+        <Button
+          type="button"
+          outline
+          color="secondary"
+          onClick={_ =>
+            this.setState(
+              state => ({ steps: [...state.steps, { text: '' }] }),
+              this.notifyChange
+            )
+          }
+        >
+          Add Another Step
+        </Button>
+      </div>
+    )
+  }
+}
