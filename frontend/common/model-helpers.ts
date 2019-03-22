18329b8248de9ddcdee82f63fd7ba4886ba17664
Ben Burwell <ben@benburwell.com>
2019-03-22 11:26:00 -0400

    Move defaultUndefined usage to uiToJSON

--- frontend/common/model-helpers.ts
+++ frontend/common/model-helpers.ts
@@ -1,5 +1,6 @@
 import * as _ from 'lodash'
 import { UserJSON } from '../models/user'
+import { defaultUndefined } from './textutils'
 
 export const getName = (user: UserJSON): string =>
   user.name ? user.name : user.username
@@ -20,4 +21,10 @@ export const jsonToUI = <T>(json: T): UITrackable<T> => ({
   original: json
 })
 
-export const uiToJSON = <T>(ui: UITrackable<T>): T => ui.json
+export const uiToJSON = <T>(ui: UITrackable<T>): T => {
+  const val = { ...ui.json }
+  _.each(_.keys(val), k => {
+    val[k] = defaultUndefined(val[k])
+  })
+  return val
+}
