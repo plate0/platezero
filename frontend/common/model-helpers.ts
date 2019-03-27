8181717e8bfd2a8c1840100fad8db8180130771e
Ben Burwell <github@benburwell.com>
2019-03-27 13:02:16 -0400

    Enable removing stuff when editing (#44)
    Can't have `null`, need it to be `undefined`.

--- frontend/common/model-helpers.ts
+++ frontend/common/model-helpers.ts
@@ -6,5 +6,6 @@ export const getName = (user: UserJSON): string =>
   user.name ? user.name : user.username
 
 export const normalize = <T extends {}>(model: T): T =>
-  pickBy(model, (val: any) => val !== '') as T
-
+  pickBy(model, (val: any) =>
+    _.isString(val) ? _.trim(val) !== '' && !_.isNil(val) : !_.isNil(val)
+  ) as T
