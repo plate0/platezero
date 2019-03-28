047025df9f6e2e3a41ba87cf9742501b8d994d30
Ben Burwell <ben@benburwell.com>
2019-03-27 22:32:28 -0400

    Refine statbar

--- frontend/components/StatBar.tsx
+++ frontend/components/StatBar.tsx
@@ -1,28 +1,24 @@
 import React from 'react'
 import { Row, Col } from 'reactstrap'
 
-interface StatProps {
+interface Stat {
   name: string
   icon: string
   value: string
 }
 
-const Stat = (props: StatProps) => (
-  <Col className="my-2 text-center">
-    <i className={`fal fa-${props.icon} fa-2x`} />
-    <div className="text-uppercase">{props.name}</div>
-    <div>{props.value}</div>
-  </Col>
-)
-
-export const StatBar = (props: { stats: StatProps[] }) => {
+export const StatBar = (props: { stats: Stat[] }) => {
   if (!props.stats.length) {
     return null
   }
   return (
     <Row className="border-top border-bottom align-items-center">
       {props.stats.map((stat, key) => (
-        <Stat key={key} {...stat} />
+        <Col key={key} className="my-2 text-center">
+          <i className={`fal fa-${stat.icon} fa-2x`} />
+          <div className="text-uppercase">{stat.name}</div>
+          <div>{stat.value}</div>
+        </Col>
       ))}
     </Row>
   )
