8fc46898c97203129912eb0bb8f28231851a5c9f
Ethan Mick <ethan@ethanmick.com>
2019-03-27 22:12:05 -0400

    Sort Recipes on User Page (#52)

--- frontend/server/api/users/user/index.ts
+++ frontend/server/api/users/user/index.ts
@@ -14,11 +14,21 @@ export interface UserRequest extends Request {
 }
 
 r.get('/', async (req: UserRequest, res) => {
-  return res.json(
-    await req.user.reload({
-      include: [{ model: Recipe, limit: 20 }]
+  try {
+    const recipes = await Recipe.findAll({
+      where: { user_id: req.user.id },
+      order: [['updated_at', 'DESC']],
+      limit: 20
     })
-  )
+    const user = await req.user.reload()
+    const json = {
+      ...user.toJSON(),
+      recipes
+    }
+    return res.json(json)
+  } catch (err) {
+    return internalServerError(res, err)
+  }
 })
 
 r.get('/recipes', async (req: UserRequest, res) => {
