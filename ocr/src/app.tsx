ba27862592b18b09e00dcc223be1691050fcee47
Ethan Mick <ethan@ethanmick.com>
2019-03-31 20:42:12 -0400

    Pure canvas

--- ocr/src/app.tsx
+++ ocr/src/app.tsx
@@ -1,142 +1,188 @@
 import * as React from 'react'
-declare var fabric: any
-
-var image
+import * as ReactDOM from 'react-dom'
 
 export class App extends React.Component<undefined, undefined> {
   constructor(props) {
     super(props)
     this.state = {}
-    this.export = this.export.bind(this)
   }
-  componentDidMount() {
-    const canvas = new fabric.Canvas('c')
-    fabric.Image.fromURL('recipe.jpg', img => {
-      image = img
-      img.scale(0.1)
-      img.selectable = false
-      canvas.add(img)
-      canvas.centerObject(img)
 
-      setTimeout(() => {
-        canvas.renderAll()
+  public componentDidMount() {
+    const canvas = ReactDOM.findDOMNode(this.refs.canvas)
+    const ctx = canvas.getContext('2d')
+    this.setState({ ctx })
+    canvas.width = 800
+    canvas.height = 600
 
-        setTimeout(() => {
-          var data = img.toDataURL({
-            format: 'jpeg',
-            left: 0,
-            top: 0,
-            width: 500,
-            height: 100
-          })
-          data = data.replace(/^data:image\/jpeg;base64,/, '')
-          console.log(data)
-          require('fs').writeFileSync('out.png', data, 'base64')
-        }, 1000)
-      }, 5000)
-    })
+    var gkhead = new Image()
 
-    canvas.on('mouse:wheel', ({ e }) => {
-      console.log(e)
-      if (e.ctrlKey) {
-        var delta = e.deltaY
-        var pointer = canvas.getPointer(e)
-        var zoom = canvas.getZoom()
-        zoom = zoom - delta / 100
-        if (zoom > 20) zoom = 20
-        if (zoom < 1) zoom = 1
-        canvas.zoomToPoint({ x: e.offsetX, y: e.offsetY }, zoom)
-        e.preventDefault()
-        e.stopPropagation()
-      } else {
-        image.top -= e.deltaY
-        image.left -= e.deltaX
-        canvas.renderAll()
-        e.preventDefault()
-        e.stopPropagation()
+    window.onload = function() {
+      var ctx = canvas.getContext('2d')
+      trackTransforms(ctx)
+
+      function redraw() {
+        // Clear the entire canvas
+        var p1 = ctx.transformedPoint(0, 0)
+        var p2 = ctx.transformedPoint(canvas.width, canvas.height)
+        ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y)
+
+        ctx.save()
+        ctx.setTransform(1, 0, 0, 1, 0, 0)
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.restore()
+
+        ctx.drawImage(gkhead, 0, 0)
       }
-    })
+      redraw()
 
-    window.addEventListener('resize', resizeCanvas, false)
+      var lastX = canvas.width / 2,
+        lastY = canvas.height / 2
 
-    function resizeCanvas() {
-      canvas.setHeight(window.innerHeight)
-      canvas.setWidth(window.innerWidth)
-      canvas.renderAll()
+      var dragStart, dragged
+
+      canvas.addEventListener(
+        'mousedown',
+        function(evt) {
+          document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect =
+            'none'
+          lastX = evt.offsetX || evt.pageX - canvas.offsetLeft
+          lastY = evt.offsetY || evt.pageY - canvas.offsetTop
+          dragStart = ctx.transformedPoint(lastX, lastY)
+          dragged = false
+        },
+        false
+      )
+
+      canvas.addEventListener(
+        'mousemove',
+        function(evt) {
+          lastX = evt.offsetX || evt.pageX - canvas.offsetLeft
+          lastY = evt.offsetY || evt.pageY - canvas.offsetTop
+          dragged = true
+          if (dragStart) {
+            var pt = ctx.transformedPoint(lastX, lastY)
+            ctx.translate(pt.x - dragStart.x, pt.y - dragStart.y)
+            redraw()
+          }
+        },
+        false
+      )
+
+      canvas.addEventListener(
+        'mouseup',
+        function(evt) {
+          dragStart = null
+          if (!dragged) zoom(evt.shiftKey ? -1 : 1)
+        },
+        false
+      )
+
+      var scaleFactor = 1.1
+
+      var zoom = function(clicks) {
+        var pt = ctx.transformedPoint(lastX, lastY)
+        ctx.translate(pt.x, pt.y)
+        var factor = Math.pow(scaleFactor, clicks)
+        ctx.scale(factor, factor)
+        ctx.translate(-pt.x, -pt.y)
+        redraw()
+      }
+
+      var handleScroll = function(evt) {
+        var delta = evt.wheelDelta
+          ? evt.wheelDelta / 40
+          : evt.detail
+          ? -evt.detail
+          : 0
+        if (delta) zoom(delta)
+        return evt.preventDefault() && false
+      }
+
+      canvas.addEventListener('DOMMouseScroll', handleScroll, false)
+      canvas.addEventListener('mousewheel', handleScroll, false)
     }
 
-    // resize on init
-    resizeCanvas()
+    gkhead.src = 'recipe.jpg'
 
-    var mousex = 0
-    var mousey = 0
-    var crop = false
-    var r = document.getElementById('c').getBoundingClientRect()
-    var pos = [0, 0]
-    pos[0] = r.left
-    pos[1] = r.top
-    // box?
-    var el = new fabric.Rect({
-      fill: 'transparent',
-      originX: 'left',
-      originY: 'top',
-      stroke: '#333',
-      strokeDashArray: [2, 2],
-      opacity: 1,
-      width: 200,
-      height: 80,
-      selectable: false
-    })
-    canvas.add(el)
-    el.bringToFront()
+    // Adds ctx.getTransform() - returns an SVGMatrix
+    // Adds ctx.transformedPoint(x,y) - returns an SVGPoint
+    function trackTransforms(ctx) {
+      var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+      var xform = svg.createSVGMatrix()
+      ctx.getTransform = function() {
+        return xform
+      }
 
-    canvas.on('mouse:down', ({ e }) => {
-      console.log(e)
-      console.log(canvas.getPointer(e))
-      const { x, y } = canvas.getPointer(e)
-      el.left = x
-      el.top = y
-      //mousex = e.pageX
-      mousey = e.pageY
-      canvas.renderAll()
-      el.bringToFront()
-      console.log('wtf', el)
-    })
-  }
+      var savedTransforms = []
+      var save = ctx.save
+      ctx.save = function() {
+        savedTransforms.push(xform.translate(0, 0))
+        return save.call(ctx)
+      }
 
-  export() {
-    var data = image.toDataURL({
-      format: 'jpeg',
-      left: 0,
-      top: 0,
-      width: 500,
-      height: 100
-    })
-    data = data.replace(/^data:image\/jpeg;base64,/, '')
-    require('fs').writeFileSync('out.png', data, 'base64')
+      var restore = ctx.restore
+      ctx.restore = function() {
+        xform = savedTransforms.pop()
+        return restore.call(ctx)
+      }
+
+      var scale = ctx.scale
+      ctx.scale = function(sx, sy) {
+        xform = xform.scaleNonUniform(sx, sy)
+        return scale.call(ctx, sx, sy)
+      }
+
+      var rotate = ctx.rotate
+      ctx.rotate = function(radians) {
+        xform = xform.rotate((radians * 180) / Math.PI)
+        return rotate.call(ctx, radians)
+      }
+
+      var translate = ctx.translate
+      ctx.translate = function(dx, dy) {
+        xform = xform.translate(dx, dy)
+        return translate.call(ctx, dx, dy)
+      }
+
+      var transform = ctx.transform
+      ctx.transform = function(a, b, c, d, e, f) {
+        var m2 = svg.createSVGMatrix()
+        m2.a = a
+        m2.b = b
+        m2.c = c
+        m2.d = d
+        m2.e = e
+        m2.f = f
+        xform = xform.multiply(m2)
+        return transform.call(ctx, a, b, c, d, e, f)
+      }
+
+      var setTransform = ctx.setTransform
+      ctx.setTransform = function(a, b, c, d, e, f) {
+        xform.a = a
+        xform.b = b
+        xform.c = c
+        xform.d = d
+        xform.e = e
+        xform.f = f
+        return setTransform.call(ctx, a, b, c, d, e, f)
+      }
+
+      var pt = svg.createSVGPoint()
+      ctx.transformedPoint = function(x, y) {
+        pt.x = x
+        pt.y = y
+        return pt.matrixTransform(xform.inverse())
+      }
+    }
   }
 
   render() {
-    setTimeout(() => {
-      const canvas = new fabric.Canvas('c')
-      // create a rectangle object
-      var rect = new fabric.Rect({
-        left: 100,
-        top: 100,
-        fill: 'red',
-        width: 20,
-        height: 20
-      })
-
-      // "add" rectangle onto canvas
-      canvas.add(rect)
-      canvas.renderAll()
-    })
     return (
       <div>
         <h2>Welcome to React with Typescript!</h2>
         <button onClick={this.export}>Export</button>
-        <canvas id="c" />
+        <canvas ref="canvas" style={{ width: '100%' }} />
       </div>
     )
   }
