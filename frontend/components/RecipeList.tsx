a0063117f7347eb2db1b579fa23dc2a78b26b0db
Ben Burwell <ben@benburwell.com>
2019-04-05 09:24:30 -0400

    Make user and recipe list pages more distinct
    The user detail page and the recipe list page were previously very
    similar, but I think they serve very different purposes. The user detail
    page is nice for being able to see who a person is and what they've been
    up to in the kitchen. On the recipe page, I need a very concise way to
    to look through potentially hundreds of recipes easily.
    
    Previously, on the recipe list page, we were devoting way too much space
    to each individual recipe making it pretty unusable on small screen
    sizes in particular. I'm certain we'll want to tweak this more after
    playing with it in prod, but I think it's at least a step in the right
    direction.

--- frontend/components/RecipeList.tsx
+++ frontend/components/RecipeList.tsx
@@ -1,31 +1,24 @@
 import React from 'react'
-import { Col, Row } from 'reactstrap'
+import { Col, Row, ListGroup, ListGroupItem } from 'reactstrap'
 import * as _ from 'lodash'
 
 import { UserJSON, RecipeJSON } from '../models'
-import { RecipeCard } from './RecipeCard'
 import { Link } from '../routes'
 import { getName } from '../common/model-helpers'
 import { IfLoggedIn } from './IfLoggedIn'
+import { RecipeListBlankslate } from './RecipeListBlankslate'
 
 export interface RecipesProps {
   user: UserJSON
   recipes: RecipeJSON[]
-  seeAll?: boolean
-  className?: string
 }
 
-export const RecipeList = ({
-  className,
-  recipes,
-  seeAll,
-  user
-}: RecipesProps) => {
+export const RecipeList = ({ recipes, user }: RecipesProps) => {
   const me = <h2 className="m-0">Your Recipes</h2>
   const not = <h2 className="m-0">{getName(user)}&#8217;s Recipes</h2>
   return (
-    <section className={className ? className : ''}>
-      <Row className="align-items-center border-bottom pb-1">
+    <section className="my-3">
+      <Row className="align-items-center">
         <Col>
           <IfLoggedIn username={user.username} else={not}>
             {me}
@@ -46,132 +39,34 @@ export const RecipeList = ({
           </Col>
         </IfLoggedIn>
       </Row>
-      <Row>
-        {!recipes.length && (
-          <Col>
-            <RecipeListBlankslate username={user.username} />
-          </Col>
-        )}
-        {seeAll ? (
-          <PreviewRecipeList recipes={recipes} user={user} />
-        ) : (
-          <FullRecipeList recipes={recipes} user={user} />
-        )}
-      </Row>
+      {!recipes.length && <RecipeListBlankslate username={user.username} />}
+      <ListGroup className="mt-3">
+        {recipes.map(recipe => {
+          const style = recipe.image_url
+            ? {
+                backgroundImage: `url(${recipe.image_url})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundBlendMode: 'overlay',
+                backgroundColor: 'rgba(255, 255, 255, 0.85)'
+              }
+            : undefined
+          return (
+            <ListGroupItem key={recipe.id} style={style}>
+              <div>
+                <Link route={`/${user.username}/${recipe.slug}`}>
+                  <a className="text-dark stretched-link">
+                    <strong>{recipe.title}</strong>
+                  </a>
+                </Link>
+              </div>
+              <div className="small text-muted text-truncate">
+                {recipe.description || 'by ' + getName(recipe.owner)}
+              </div>
+            </ListGroupItem>
+          )
+        })}
+      </ListGroup>
     </section>
-  )
-}
-
-export const RecipeListBlankslate = (props: { username: string }) => (
-  <div className="bg-light text-secondary text-center rounded p-5 mt-3">
-    <h1>No recipes yet :(</h1>
-    <IfLoggedIn username={props.username}>
-      <Link route="/recipes/new">
-        <a className="btn btn-primary">Create your first!</a>
-      </Link>
-    </IfLoggedIn>
-  </div>
-)
-
-const RecipeCol = ({
-  className,
-  children
-}: {
-  className?: string
-  children: any
-}) => (
-  <Col xs="12" md="4" xl="3" className={`mt-3 ${className || ''}`}>
-    {children}
-  </Col>
-)
-
-const SeeMore = ({ username }: { username: string }) => (
-  <div
-    style={{
-      backgroundColor: 'var(--light)',
-      height: 180,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}
-  >
-    <Link to={`/${username}/recipes`}>
-      <a className="text-secondary stretched-link">See more&hellip;</a>
-    </Link>
-  </div>
-)
-
-const PreviewRecipeList = ({
-  recipes,
-  user
-}: {
-  recipes: RecipeJSON[]
-  user: UserJSON
-}) => {
-  const smalls = mapToPreviewRow(recipes, 3, 'd-xl-none', user.username)
-  const larges = mapToPreviewRow(recipes, 4, 'd-none d-xl-block', user.username)
-  return (
-    <>
-      {smalls}
-      {larges}
-    </>
-  )
-}
-
-const mapToPreviewRow = (
-  recipes: RecipeJSON[],
-  max: number,
-  className: string,
-  username: string
-) => {
-  return _.reject(
-    _.map(recipes, (r, idx) => {
-      const cardCol = (
-        <RecipeCol className={className} key={idx}>
-          <RecipeCard
-            title={r.title}
-            slug={r.slug!}
-            image_url={r.image_url}
-            username={username}
-          />
-        </RecipeCol>
-      )
-      if (idx < max - 1) {
-        return cardCol
-      }
-      if (idx === max - 1) {
-        return _.size(recipes) > max ? (
-          <RecipeCol className={className} key={idx}>
-            <SeeMore username={username} />
-          </RecipeCol>
-        ) : (
-          cardCol
-        )
-      }
-    }),
-    _.isUndefined
-  )
-}
-
-export const FullRecipeList = ({
-  user,
-  recipes
-}: {
-  user: UserJSON
-  recipes: RecipeJSON[]
-}) => {
-  return (
-    <>
-      {recipes.map(r => (
-        <RecipeCol key={r.slug}>
-          <RecipeCard
-            title={r.title}
-            slug={r.slug!}
-            image_url={r.image_url}
-            username={user.username}
-          />
-        </RecipeCol>
-      ))}
-    </>
   )
 }
