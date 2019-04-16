99edd0bbd57c03370da4a7bd038251f54d413912
Ethan Mick <ethan@ethanmick.com>
2019-04-16 15:06:16 -0400

    iOS status bar black-translucent

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
-            content="default"
+            content="black-translucent"
           />
           <link
             rel="apple-touch-icon"
