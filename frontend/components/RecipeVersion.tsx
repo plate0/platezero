8dc58bdcbba40d9ef765aea534795471dcd7f42f
Ben Burwell <ben@benburwell.com>
2019-04-23 09:11:22 -0400

    Handle updating of recipe image

--- frontend/components/RecipeVersion.tsx
+++ frontend/components/RecipeVersion.tsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import * as moment from 'moment'
+import * as _ from 'lodash'
 import { Row, Col } from 'reactstrap'
 import { Timestamp, humanize } from './Timestamp'
 import { IngredientLists } from './IngredientLists'
 import { ProcedureLists } from './ProcedureLists'
+import { AlertErrors } from './AlertErrors'
 import { EditableImage } from './EditableImage'
 import { RecipeVersionVitals } from './RecipeVersionVitals'
 import { Markdown } from './Markdown'
 import { RecipeVersionJSON } from '../models/recipe_version'
 import { getName } from '../common/model-helpers'
+import { api, getErrorMessages } from '../common/http'
+import { UserContext } from '../context/UserContext'
 
 export const RecipeVersion = (props: { recipeVersion: RecipeVersionJSON }) => {
+  const [imageUrl, setImageUrl] = useState(props.recipeVersion.recipe.image_url)
+  const [updateImageErrors, setUpdateImageErrors] = useState(null)
+  const userContext = useContext(UserContext)
+
   const v = props.recipeVersion
   const prevUrl = v.parent_recipe_version_id
     ? `/${v.recipe.owner.username}/${v.recipe.slug}/versions/${
         v.parent_recipe_version_id
       }`
     : undefined
+
+  const onRecipeImageEdit = async (image_url: string) => {
+    try {
+      await api.patchRecipe(v.recipe.slug, { image_url })
+      setImageUrl(image_url)
+    } catch (err) {
+      setUpdateImageErrors(getErrorMessages(err))
+    }
+  }
+
   return (
     <>
       <details className="mb-3">
@@ -42,13 +60,21 @@ export const RecipeVersion = (props: { recipeVersion: RecipeVersionJSON }) => {
       <RecipeVersionVitals recipeVersion={v} />
       <Row>
         <Col xs="12" md="6" lg="4">
-          {v.recipe.image_url && (
-            <EditableImage
-              src={v.recipe.image_url}
-              alt={`Picture of ${v.recipe.title}`}
+          <EditableImage
+            src={imageUrl}
+            onUpdate={onRecipeImageEdit}
+            canEdit={
+              v.recipe.owner.username === _.get(userContext, 'user.username')
+            }
+          >
+            <img
+              className="w-100"
+              src={imageUrl}
               itemProp="image"
+              alt={`Picture of ${v.recipe.title}`}
             />
-          )}
+          </EditableImage>
+          <AlertErrors errors={updateImageErrors} />
           <h2>Ingredients</h2>
           <IngredientLists lists={v.ingredientLists} />
         </Col>
