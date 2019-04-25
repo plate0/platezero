4b498bbdf8d30858d91b624cad91ee9111dedb37
Ben Burwell <ben@benburwell.com>
2019-04-25 13:47:24 -0400

    Handle toggle() called after unmount
    When the user mouses off the tooltip target, there is a short delay
    before toggle() is called to hide the tooltip. In the interim, it's
    possible that the component is unmounted, e.g. during navigation to
    another page.
    
    As a hack around this, we set isMounted to true, and as a cleanup
    effect, set isMounted to false. Then when toggle gets called, we can
    check whether we should actually be mutating the state based on whether
    the component is still mounted.

--- frontend/components/Tooltip.tsx
+++ frontend/components/Tooltip.tsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Tooltip as RsTooltip } from 'reactstrap'
 
 export const Tooltip = ({ children, text, id }) => {
   const [open, setOpen] = useState(false)
+
+  const isMounted = useRef(true)
+  useEffect(
+    () => () => {
+      isMounted.current = false
+    },
+    []
+  )
+
+  const toggle = () => {
+    // only mutate state if component is mounted
+    if (isMounted.current) {
+      setOpen(!open)
+    }
+  }
+
   return (
     <>
       {children}
-      <RsTooltip isOpen={open} target={id} toggle={() => setOpen(!open)}>
+      <RsTooltip isOpen={open} target={id} toggle={toggle}>
         {text}
       </RsTooltip>
     </>
