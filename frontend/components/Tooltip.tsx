ffc21e9136b420e2ee3c6bf77b567b75b58ee842
Ethan Mick <ethan@ethanmick.com>
2019-04-25 14:01:30 -0400

    Updated hash

--- frontend/components/Tooltip.tsx
+++ frontend/components/Tooltip.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 import { Tooltip as RsTooltip } from 'reactstrap'
+import { hash } from '../common'
 
-export const Tooltip = ({ children, text, id }) => {
+export const Tooltip = ({ children, tip }) => {
   const [open, setOpen] = useState(false)
+  const [id] = useState(hash(tip))
   return (
     <>
-      {children}
+      <div id={id}>{children}</div>
       <RsTooltip isOpen={open} target={id} toggle={() => setOpen(!open)}>
-        {text}
+        {tip}
       </RsTooltip>
     </>
   )
