48a9df2bb1645b742965531ddd1ba544005f5e38
Brian Dobby <brian@blivot.com>
2019-04-29 16:22:18 +0100

    Added debug logging and more tests

--- frontend/common/amount.test.ts
+++ frontend/common/amount.test.ts
@@ -1,8 +1,36 @@
 import {parseAmount} from './amount'
 
 describe('parseAmount', () => {
-    it('should detect whole number', () => {
+    it('should detect a whole number', () => {
       expect(parseAmount('42 tons sand')).toEqual([42, 1, 'tons sand'])
+    })
+
+    it('should detect a whole number followed by a number that is not a fraction but part of the text', () => {
+      expect(parseAmount('1 400g can worms')).toEqual([1, 1, '400g can worms'])
+    })
+
+    it('should detect a whole number followed by a fraction', () => {
+      expect(parseAmount('1 1/2 eggs')).toEqual([3, 2, 'eggs'])
+    })
+
+    it('should detect a fraction', () => {
+      expect(parseAmount('1/2 lb tuppeny rice')).toEqual([1, 2, 'lb tuppeny rice'])
+    })
+
+    it('should detect a decimal with a leading zero', () => {
+      expect(parseAmount('0.5 lb tuppeny rice')).toEqual([1, 2, 'lb tuppeny rice'])
+    })
+
+    it('should detect a decimal with a leading non-zero value', () => {
+      expect(parseAmount('1.5 lb tuppeny rice')).toEqual([3, 2, 'lb tuppeny rice'])
+    })
+
+    it('should detect a decimal with no leading zero', () => {
+      expect(parseAmount('.5 lb tuppeny rice')).toEqual([1, 2, 'lb tuppeny rice'])
+    })
+
+    it('should not fail if no amount is detected', () => {
+      expect(parseAmount('a whole bunch of other stuff')).toEqual([undefined, undefined, 'a whole bunch of other stuff'])
     })
 
 })
--- frontend/common/amount.ts
+++ frontend/common/amount.ts
@@ -1,24 +1,36 @@
 import * as _ from 'lodash'
 import Fraction from 'fraction.js'
+import {logger} from './logger'
 
 export const parseAmount = (text: string): [number, number, string] => {
   try {
+      logger.debug('Parsing %s', text)
     // first try to match a decimal
     const decResults = text.match(/^(\d*\.\d+?)/)
     if (decResults) {
+      logger.debug('matched a decimal')
       const f = new Fraction(decResults[0])
       const rest = text.substring(_.size(decResults[0]))
       return [f.n, f.d, _.trim(rest)]
     }
-    // next, if no decimal was found, try to match a whole number or whole
-    // number + fraction
-    const results = text.match(/^((\d+\s+)?\d+(\/\d+)?)/)
+    // next, if no decimal was found, try to match a whole number + fraction
+    const fracResults = text.match(/^((\d+\s+)?(\d+\/\d+))/)
+    if (fracResults) {
+      logger.debug('matched a whole number + fraction')
+      const f = new Fraction(fracResults[0])
+      const rest = text.substring(_.size(fracResults[0]))
+      return [f.n, f.d, _.trim(rest)]
+    }
+    // next, if no fraction was found, try to match a whole number
+    const results = text.match(/^(\d+)/)
     if (results) {
+      logger.debug('matched a whole number')
       const f = new Fraction(results[0])
       const rest = text.substring(_.size(results[0]))
       return [f.n, f.d, _.trim(rest)]
     }
-  } catch {}
+  } catch (err) {logger.debug('Caught error: ', new Error(err))}
+  logger.debug('no match')
   return [undefined, undefined, _.trim(text)]
 }
 
