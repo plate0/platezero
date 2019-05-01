f8b9b691323ef67fc4476b994929f5bfd48340e7
Ben Burwell <ben@benburwell.com>
2019-05-01 10:40:13 -0400

    Add metrics
    Start a framework which lets us easily collect metrics from our prod
    instances using prometheus.
    
    There is a new endpoint, /_metrics, which can be used to scrape the
    current values. Right now, we're just collecting very baseline
    information. I'd like to get this up and running so that we can easily
    instrument both business and technical metrics that we're interested in.
    For now, simply measuring API requests will give us an idea of how much
    PZ is being used. In the near future, we can add things like tracking
    new users signing up, loading particular pages we're interested in (such
    as the recipe view page), etc.
    
    Once this is live, my next step will be to set up a dashboard that
    scrapes metrics from the new endpoint and lets us look at them on a
    Grafana dashboard.

--- frontend/server/index.ts
+++ frontend/server/index.ts
@@ -1,23 +1,55 @@
 import * as express from 'express'
 import * as next from 'next'
 import { join } from 'path'
+import * as prom from 'prom-client'
 const { routes } = require('../routes')
 const { sequelize } = require('../models')
 import { api } from './api'
 import { config } from './config'
+import { HttpStatus } from '../common/http-status'
 
 const app = next({ dev: config.dev })
 const handler = routes.getRequestHandler(app)
 
 app.prepare().then(() => {
+  prom.collectDefaultMetrics({ timeout: 5000 })
+
   const server = express()
 
   server.use(express.static(join(__dirname, '..', 'static')))
-  server.use('/api', api)
+
+  const apiLatency = new prom.Histogram({
+    name: 'pz_api_latency',
+    help: 'API latency',
+    labelNames: ['method', 'code']
+  })
+  server.use(
+    '/api',
+    (req, res, next) => {
+      const finish = apiLatency.startTimer({ method: req.method })
+      res.on('finish', () => {
+        finish({ code: res.statusCode })
+      })
+      next()
+    },
+    api
+  )
+
   server.use(
     '/announcements',
     express.static(join(__dirname, '..', 'announcements'))
   )
+  server.get('/_metrics', (req, res) => {
+    const auth = req.headers.authorization
+    if (!auth) {
+      return res.status(HttpStatus.Unauthorized).end()
+    }
+    const [scheme, token] = auth.split(' ')
+    if (!scheme || token !== 'eereexisahJeishiehahqu7eitei5eis') {
+      return res.status(HttpStatus.Unauthorized).end()
+    }
+    return res.end(prom.register.metrics())
+  })
   server.use(handler)
 
   sequelize
