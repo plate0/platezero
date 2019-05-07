7be31a981fa2ac8f4fbb5c20bb46e3597e136ccc
Ethan Mick <ethan@ethanmick.com>
2019-05-07 10:37:11 -0400

    Add delicious magazine importer

--- frontend/server/importer/index.ts
+++ frontend/server/importer/index.ts
@@ -8,6 +8,7 @@ import { NYTCooking } from './nyt-cooking'
 import { GenericHTML } from './generic-html-importer'
 import { FoodNetwork } from './food-network'
 import { Popsugar } from './popsugar'
+import { DeliciousMagazine } from './deliciousmagazine'
 import { Reddit } from './reddit'
 
 const importers = mapValues(
@@ -17,7 +18,8 @@ const importers = mapValues(
     'dlink.blueapron.com': BlueApron,
     'www.seriouseats.com': SeriousEats,
     'www.foodnetwork.com': FoodNetwork,
-    'www.popsugar.com': Popsugar
+    'www.popsugar.com': Popsugar,
+    'www.deliciousmagazine.co.uk': DeliciousMagazine
   },
   importer => fetch(toHTML(dom(importer)))
 )
