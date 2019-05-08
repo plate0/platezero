f222f86e39364adf6a922eb464e79794c7852c3e
Brian Dobby <brian@blivot.com>
2019-05-08 14:50:06 +0100

    Fix by @benburwell

--- frontend/components/ProcedureLists.tsx
+++ frontend/components/ProcedureLists.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Row, Col, Badge } from 'reactstrap'
 
 import { ProcedureListJSON } from '../models'
-import { InlineMarkdown } from './InlineMarkdown'
+import { Markdown } from './Markdown'
 
 export const ProcedureLists = ({ lists }: { lists: ProcedureListJSON[] }) => (
   <>
@@ -13,51 +13,38 @@ export const ProcedureLists = ({ lists }: { lists: ProcedureListJSON[] }) => (
   </>
 )
 
-const ProcedureList = ({ list, index }: { list: ProcedureListJSON, index: number }) => (
+const ProcedureList = ({ list }: { list: ProcedureListJSON }) => (
   <div className="mb-3">
-    {list.name && (<p className="font-weight-bold border-bottom"><ItemNo value={index+1}/> {list.name}</p>)}
+    {list.name && <p className="font-weight-bold border-bottom">{list.name}</p>}
     {list.lines.map((l, key) => (
-      <div
+      <Row
         key={key}
         itemProp="recipeInstructions"
         itemScope={true}
         itemType="http://schema.org/HowToStep"
       >
-        <Row>
-          {l.image_url && (
-            <Col xs="12" className="px-0 px-sm-3">
-              <img
-                className="w-100 mb-3 p-0 d-print-none"
-                src={l.image_url}
-                itemProp="exampleOfWork"
-              />
-            </Col>
-          )}
-          {l.title && (
-            <Col xs="12">
-              <div className="mb-3">
-                <h5 className="border-bottom pb-2 d-flex align-items-center">
-                  <span itemProp="headline">{l.title}</span>
-                </h5>
-              </div>
-            </Col>
-          )}
-          <Col xs="12" itemProp="text">
-          {list.name || (
-                  <ItemNo value={key + 1}/>
-                 )}
-           <InlineMarkdown source={l.text} />
+        {l.image_url && (
+          <Col xs="12">
+            <img
+              className="w-100 mb-3 p-0 d-print-none"
+              src={l.image_url}
+              itemProp="exampleOfWork"
+            />
           </Col>
-        </Row>
-      </div>
+        )}
+        <Col xs="1">
+          <ItemNo value={key + 1} />
+        </Col>
+        <Col xs="11" itemProp="text">
+          {l.title && (
+            <h5>
+              <span itemProp="headline">{l.title}</span>
+            </h5>
+          )}
+          <Markdown source={l.text} />
+        </Col>
+      </Row>
     ))}
-    <style jsx>
-    {`
-      .badge + p {
-        display: inline;
-      }
-    `}
-  </style>
   </div>
 )
 
