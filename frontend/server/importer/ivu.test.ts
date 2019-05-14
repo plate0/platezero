18ed404d3b707f809e0240c791c86ef88e5aa6c2
Brian Dobby <brian@blivot.com>
2019-05-14 15:52:52 +0100

    Formatting changes

--- frontend/server/importer/ivu.test.ts
+++ frontend/server/importer/ivu.test.ts
@@ -1,13 +1,12 @@
-import { Ivu } from './ivu'
+import { IVU } from './ivu'
 import { dom } from './html'
 import { readFileSync } from 'fs'
 
-const recipe =
-  'test/assets/ivu.org/sauteed-peppers.html'
+const recipe = 'test/assets/ivu.org/sauteed-peppers.html'
 
 describe('IVU', () => {
   let source: string
-  let importer = dom(Ivu)
+  let importer = dom(IVU)
 
   beforeEach(() => {
     source = readFileSync(recipe, { encoding: 'utf8' })
@@ -26,31 +25,88 @@ describe('IVU', () => {
   test('get yield', async () => {
     const o = await importer(source)
     const Yield = o['yield']
-    expect(Yield).toEqual('1')
+    expect(Yield).toEqual('Serving Size: 1')
   })
 
   test('get ingredient lists', async () => {
-      const { ingredient_lists: list } = await importer(source)
-      expect(list).toHaveLength(1)
-      expect(list[0].lines).toEqual([
-            {preparation: undefined, optional: false, quantity_numerator: 3,         quantity_denominator: 1,         unit: 'tbsp',      name: 'olive oil'},
-            {preparation: undefined, optional: false, quantity_numerator: 1,         quantity_denominator: 1,         unit: undefined,   name: 'small red onion -- or 1/2 Vidalia'},
-            {preparation: undefined, optional: false, quantity_numerator: 3,         quantity_denominator: 1,         unit: undefined,   name: 'bell peppers -- (any color or colors'},
-            {preparation: undefined, optional: false, quantity_numerator: 2,         quantity_denominator: 1,         unit: undefined,   name: 'garlic cloves -- sliced (or powder)'},
-            {preparation: undefined, optional: false, quantity_numerator: 1,         quantity_denominator: 4,         unit: 'c',         name: 'tomato sauce'},
-            {preparation: undefined, optional: false, quantity_numerator: 1,         quantity_denominator: 1,         unit: 'tbsp',      name: 'balsamic vinegar'},
-            {preparation: undefined, optional: false, quantity_numerator: 1,         quantity_denominator: 1,         unit: 'tbsp',      name: 'marjoram -- (1 t dried)'},
-            {preparation: undefined, optional: false, quantity_numerator: undefined, quantity_denominator: undefined, unit: undefined,   name: 'salt and pepper -- to taste'}
-            ])
-      })
-      
-      test('get procedure lists', async () => {
-          const { procedure_lists: lists } = await importer(source)
-          expect(lists).toHaveLength(1)
-          expect(lists[0].lines).toHaveLength(1)
-          expect(lists[0].lines[0]['image_url']).toBeUndefined()
-          expect(lists[0].lines[0]['title']).toBeUndefined()
-          expect(lists[0].lines[0]['text']).toMatch(/^Quarter onion and slice thinly./)
-        })
+    const { ingredient_lists: list } = await importer(source)
+    expect(list).toHaveLength(1)
+    expect(list[0].lines).toEqual([
+      {
+        preparation: undefined,
+        optional: false,
+        quantity_numerator: 3,
+        quantity_denominator: 1,
+        unit: 'tbsp',
+        name: 'olive oil'
+      },
+      {
+        preparation: undefined,
+        optional: false,
+        quantity_numerator: 1,
+        quantity_denominator: 1,
+        unit: undefined,
+        name: 'small red onion -- or 1/2 Vidalia'
+      },
+      {
+        preparation: undefined,
+        optional: false,
+        quantity_numerator: 3,
+        quantity_denominator: 1,
+        unit: undefined,
+        name: 'bell peppers -- (any color or colors'
+      },
+      {
+        preparation: undefined,
+        optional: false,
+        quantity_numerator: 2,
+        quantity_denominator: 1,
+        unit: undefined,
+        name: 'garlic cloves -- sliced (or powder)'
+      },
+      {
+        preparation: undefined,
+        optional: false,
+        quantity_numerator: 1,
+        quantity_denominator: 4,
+        unit: 'c',
+        name: 'tomato sauce'
+      },
+      {
+        preparation: undefined,
+        optional: false,
+        quantity_numerator: 1,
+        quantity_denominator: 1,
+        unit: 'tbsp',
+        name: 'balsamic vinegar'
+      },
+      {
+        preparation: undefined,
+        optional: false,
+        quantity_numerator: 1,
+        quantity_denominator: 1,
+        unit: 'tbsp',
+        name: 'marjoram -- (1 t dried)'
+      },
+      {
+        preparation: undefined,
+        optional: false,
+        quantity_numerator: undefined,
+        quantity_denominator: undefined,
+        unit: undefined,
+        name: 'salt and pepper -- to taste'
+      }
+    ])
+  })
 
+  test('get procedure lists', async () => {
+    const { procedure_lists: lists } = await importer(source)
+    expect(lists).toHaveLength(1)
+    expect(lists[0].lines).toHaveLength(8)
+    expect(lists[0].lines[0]['image_url']).toBeUndefined()
+    expect(lists[0].lines[0]['title']).toBeUndefined()
+    expect(lists[0].lines[0]['text']).toMatch(
+      /^Quarter onion and slice thinly./
+    )
+  })
 })
--- frontend/server/importer/ivu.ts
+++ frontend/server/importer/ivu.ts
@@ -1,15 +1,18 @@
-//import * as moment from 'moment'
+/*
+ * This is really old-style HTML with few, if any, 'id' or 'class' attributes.
+ * Notes on input format:
+ *  1. The whole recipe is contained within the single <td> of the single <tr> of a <table>
+ *  2. The parsing depends on locating elements set around the <ul> which contains the ingredients
+ *  3. The title, source, and yield are located in <p> tags before the <ul>
+ *  4. The procedures follow the <ul>, but are in a mixture of 
+ *      a) 'text' of the <td>
+ *      b) <p> tags 
+ */
 import { mapValues } from './importer'
-import {
-  //  Preheat,
-  IngredientListJSON,
-    ProcedureListJSON,
-} from '../../models'
+import { IngredientListJSON, ProcedureListJSON } from '../../models'
 import { parseAmount, parseUnit } from 'ingredient-parser'
 const TurndownService = require('turndown')
 
-const rxAmount = /^[0-9/ ½⅓⅔¼¾⅖⅗⅘⅙⅚⅐⅛⅜⅝⅞⅑⅒]+/
-		
 const title = ($: any) =>
   $('table table p font')[0]
     .children[0] // text
@@ -26,7 +29,6 @@ const getYield = ($: any) =>
   $('table table p:nth-of-type(2)')[0]
     .children[0] // text
     .data.trim()
-    .replace(/^[\w\s]+:\s/, '')
 
 const ingredient_lists = ($: any): IngredientListJSON[] => [
   {
@@ -34,29 +36,29 @@ const ingredient_lists = ($: any): IngredientListJSON[] => [
     image_url: undefined,
     lines: $('table table ul li')
       .map(function() {
-        let line = $(this)
+        let rest = $(this)
           .text()
           .trim()
         let name, quantity_numerator, quantity_denominator, unit
 
-        const hasAmount = line.match(rxAmount)
+        const hasAmount = rest.match(rxAmount)
         if (hasAmount) {
           const quantity = hasAmount[0]
           const f = parseAmount(quantity)
           quantity_numerator = f.n
           quantity_denominator = f.d
 
-          line = line.substring(quantity.length).trim()
-          const haveUnit = line.match(/\S+/)
+          rest = rest.substring(quantity.length).trim()
+          const haveUnit = rest.match(/\S+/)
           if (haveUnit) {
-           unit = parseUnit(haveUnit[0])
+            unit = parseUnit(haveUnit[0])
             if (unit) {
-              line = line.substring(haveUnit[0].length).trim()
+              rest = rest.substring(haveUnit[0].length).trim()
             }
           }
         }
 
-        name = line
+        name = rest
 
         return {
           name,
@@ -72,35 +74,41 @@ const ingredient_lists = ($: any): IngredientListJSON[] => [
 ]
 
 const procedure_lists = ($: any): ProcedureListJSON[] => {
-    const turndown = new TurndownService()
-    let ulIndex
-    const td = $('table table td')
-    // Remove elements up to and including 'ul'
-    td.children().each(function (index, element){
-        if (element.type == 'tag') {
-            let name = element.name
-            if (name == 'ul') {
-                ulIndex = index
-                return false
-            }
-        }
-        })
-      while (ulIndex >= 0) {
-          td.children().remove(':nth-child('+(1+ulIndex--)+')')  
+  const turndown = new TurndownService()
+  let ulIndex
+  const td = $('table table td')
+  // Remove child elements up to and including 'ul'
+  td.children().each(function(index, element) {
+    if (element.type == 'tag') {
+      let name = element.name
+      if (name == 'ul') {
+        ulIndex = index
+        return false
       }
-      let html = td.html()+td.children().html()
-      let md = turndown.turndown(html)
-    return [
-      {
-        name: undefined,
-        lines: [{
-            image_url: undefined,
-            title: undefined,
-            text: md
-        }]
-      }
-    ]
+    }
+  })
+  while (ulIndex >= 0) {
+    td.children().remove(':nth-child(' + (1 + ulIndex--) + ')')
   }
+  
+  let html = td.html() + td.children().html()
+  let lines = turndown
+    .turndown(html)
+    .split('\n')
+    .filter(line => line.trim().length > 0)
+  return [
+    {
+      name: undefined,
+      lines: lines.map(line => {
+        return {
+          image_url: undefined,
+          title: undefined,
+          text: line.trim()
+        }
+      })
+    }
+  ]
+}
 
 export const IVU = mapValues({
   title: title,
@@ -110,3 +118,4 @@ export const IVU = mapValues({
   procedure_lists: procedure_lists
 })
 
+const rxAmount = /^[0-9/ ½⅓⅔¼¾⅖⅗⅘⅙⅚⅐⅛⅜⅝⅞⅑⅒]+/
