01c882822942d3713dc8a5be775ebc32570a8b89
Ben Burwell <ben@benburwell.com>
2019-05-21 13:05:46 -0400

    Add link to blog in footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,38 +1,32 @@
 import React from 'react'
-import { Container, Row, Col } from 'reactstrap'
+import { Container } from 'reactstrap'
 
 export const Footer = () => (
-  <footer className="text-muted bg-light d-flex mt-auto d-print-none">
-    <Container className="align-items-center d-flex">
-      <Row className="w-100">
-        <Col xs="12" md="6">
-          <ul className="mb-0 list-unstyled">
-            <li>
-              <a href="mailto:bugs@platezero.com">
-                <i className="fal fa-bug fa-fw mr-2" />
-                Report a bug
-              </a>
-            </li>
-          </ul>
-        </Col>
-        <Col xs="12" md="6">
-          <ul className="mb-0 list-unstyled">
-            <li>
-              <a href="mailto:hello@platezero.com">
-                <i className="fal fa-envelope fa-fw mr-2" />
-                Contact us
-              </a>
-            </li>
-          </ul>
-        </Col>
-      </Row>
+  <footer className="text-muted bg-light d-flex mt-auto d-print-none py-5">
+    <Container>
+      <ul className="list-unstyled d-flex align-items-center justify-content-between">
+        <li>
+          <a href="mailto:bugs@platezero.com">
+            <i className="fal fa-bug fa-fw mr-2" />
+            Report a bug
+          </a>
+        </li>
+        <li>
+          <a href="mailto:hello@platezero.com">
+            <i className="fal fa-envelope fa-fw mr-2" />
+            Contact us
+          </a>
+        </li>
+        <li>
+          <a href="https://blog.platezero.com" target="_blank">
+            <i className="fal fa-rss fa-fw mr-2" />
+            Blog
+          </a>
+        </li>
+      </ul>
     </Container>
     <style jsx>
       {`
-        footer {
-          min-height: 200px;
-        }
-
         a {
           color: #6c757d;
         }
