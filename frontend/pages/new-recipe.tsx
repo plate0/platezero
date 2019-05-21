c4848a209ad0515843ee7ada39308273d29cc080
Ben Burwell <ben@benburwell.com>
2019-05-21 11:57:17 -0400

    Clean up add recipe landing page
    Use consistent card sizes, center content, and make wording more
    concise.

--- frontend/pages/new-recipe.tsx
+++ frontend/pages/new-recipe.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Col } from 'reactstrap'
+import { Card, CardBody } from 'reactstrap'
 import Head from 'next/head'
 import { Layout } from '../components'
 import * as _ from 'lodash'
@@ -7,33 +7,33 @@ import { Link } from '../routes'
 
 const links = [
   {
-    text: 'Write it out',
+    text: 'By Hand',
     icon: 'pen',
     color: 'rgb(119, 192, 168)',
     route: 'new-recipe-text'
   },
   {
-    text: 'From a website',
+    text: 'Website',
     icon: 'globe',
     color: 'rgb(58, 154, 217)',
     route: 'new-recipe-url'
   },
   {
-    text: 'From a picture',
+    text: 'Photo',
     icon: 'images',
     color: 'rgb(235, 113, 96)',
     route: 'new-recipe-file',
     params: { type: 'image' }
   },
   {
-    text: 'From a PDF/Doc',
+    text: 'PDF/Doc',
     icon: 'file-pdf',
     color: 'rgb(222, 77, 78)',
     route: 'new-recipe-file',
     params: { type: 'doc' }
   },
   {
-    text: 'Email it',
+    text: 'Email',
     icon: 'envelope',
     color: 'rgb(110, 158, 207)',
     route: 'mailto:importer@platezero.com'
@@ -47,56 +47,36 @@ export default class ImportRecipe extends React.Component {
         <Head>
           <title>Add a new Recipe</title>
         </Head>
-        <Row className="mt-5 mb-3">
-          <Col xs="12">
-            <h2 className="mb-0">Add a new Recipe</h2>
-            <p className="text-muted">
-              You can easily add a recipe in several ways.
-            </p>
-          </Col>
-        </Row>
-        <Row>
+        <h2 className="mt-5 text-center">Add a Recipe</h2>
+        <p className="lead text-center">Where will your recipe come from?</p>
+        <div className="mb-3 d-flex justify-content-center align-items-center flex-wrap">
           {links.map((s, key) => (
-            <Col xs="6" md="2" className="my-3" key={key}>
-              <Link to={s.route} params={s.params}>
-                <a className="import">
-                  <div
-                    className="p-3 text-center d-flex align-items-center flex-column border rounded"
-                    style={{
-                      boxShadow: '0 0 5px rgba(0, 0, 0, 0.1) inset'
-                    }}
-                  >
-                    <i
-                      className={`write fal fa-${s.icon} fa-4x rounded`}
-                      style={{ color: s.color }}
-                    />
-                    <div className="mt-2">{s.text}</div>
-                  </div>
-                </a>
-              </Link>
-            </Col>
+            <Card
+              key={key}
+              className="m-2"
+              style={{ boxShadow: '0 0 5px rgba(0, 0, 0, 0.1) inset' }}
+            >
+              <CardBody className="text-center d-relative">
+                <div className="mb-3">
+                  <i
+                    className={`fal fa-fw fa-${s.icon} fa-4x`}
+                    style={{ color: s.color }}
+                  />
+                </div>
+                <div>
+                  <Link to={s.route} params={s.params}>
+                    <a className="text-dark stretched-link">{s.text}</a>
+                  </Link>
+                </div>
+              </CardBody>
+            </Card>
           ))}
-        </Row>
-        <Row className="justify-content-center">
-          <Col xs="10" md="6" className="text-center">
-            <p className="text-muted">
-              Want to add recipes from somewhere else? A cookbook, or many
-              URL&#39;s? <a href="mailto:hello@platezero.com">Contact Us</a> for
-              help.
-            </p>
-          </Col>
-        </Row>
-        <style jsx>
-          {`
-            a.import {
-              color: #212529;
-            }
-
-            a.import:hover {
-              text-decoration: none;
-            }
-          `}
-        </style>
+        </div>
+        <p className="mx-5 text-muted text-center">
+          Want to add recipes from somewhere else, like an entire cookbook or
+          many URLs? <a href="mailto:hello@platezero.com">Contact us</a> for
+          help.
+        </p>
       </Layout>
     )
   }
