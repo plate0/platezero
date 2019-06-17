1c4f61a6cbde6959f64392b7e5d6ae2d835a4557
Brian Dobby <brian@blivot.com>
2019-06-17 15:16:01 +0100

    Convert extractors from js to ts

---
+++ frontend/server/shrimp/extractors/docx.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  load: async filename => {
+    const AdmZip = require('adm-zip')
+    // Read document.xml from docx document
+    const zip = new AdmZip(filename)
+    const xml = zip.readAsText('word/document.xml')
+    // Load xml DOM
+    const $ = cheerio.load(xml, {
+      normalizeWhitespace: true,
+      xmlMode: true
+    })
+    let out = new Array()
+    $('w\\:t').each((_i, el) => {
+      out.push($(el).text())
+    })
+    return out
+  }
+}
--- frontend/server/shrimp/extractors/pdf.ts
+++ frontend/server/shrimp/extractors/pdf.ts
@@ -1,36 +1,35 @@
+const pdfReader = require('pdfreader')
 
 module.exports = {
   load: async filename => {
-
     let p = new Promise((resolve, reject) => {
-      var pdfReader = require("pdfreader");
-      let rdr = new pdfReader.PdfReader({ debug: "true" });
+      let rdr = new pdfReader.PdfReader({ debug: 'true' })
 
-      var mapOfRows = new Map(); // indexed by [x-position, y-position]
-      var pageWidth;
+      var mapOfRows = new Map() // indexed by [x-position, y-position]
+      var pageWidth
       rdr.parseFileItems(filename, function(err, item) {
         if (err) {
-          throw new Error(err);
+          reject(err)
         } else if (!item) {
-          resolve(transform(mapOfRows));
+          resolve(transform(mapOfRows))
         } else if (item.page && item.width) {
-          pageWidth = item.width;
+          pageWidth = item.width
         } else if (item.text) {
           // accumulate text items into rows object, per line
-          let key = [calcx(item.x, pageWidth), item.y];
-          row = mapOfRows.get(key);
+          const key = [calcx(item.x, pageWidth), item.y]
+          let row = mapOfRows.get(key)
           if (!row) {
-            row = [];
+            row = []
           }
-          row.push(item.text);
-          mapOfRows.set(key, row);
+          row.push(item.text)
+          mapOfRows.set(key, row)
         }
-      });
-    });
-    let text = await p;
-    return text;
+      })
+    })
+    let text = await p
+    return text
   }
-};
+}
 
 // In order to deal with 2-column pages,
 // 'x' is assumed to be 'column 0' if it
@@ -38,7 +37,7 @@ module.exports = {
 // else 'column 1'.
 // This may be overkill and/or inadequate
 function calcx(x, pageWidth) {
-  return x >= pageWidth / 2 ? 1 : 0;
+  return x >= pageWidth / 2 ? 1 : 0
 }
 
 // Transform the map of position->text into
@@ -46,17 +45,17 @@ function calcx(x, pageWidth) {
 // such that all rows in column 0 precede all rows
 // in column 1
 function transform(mapOfRows) {
-  let out = [];
+  let out = []
   Array.from(mapOfRows.keys())
     .sort((k1, k2) => {
-      n = parseFloat(k1[0]) - parseFloat(k2[0]);
+      let n = parseFloat(k1[0]) - parseFloat(k2[0])
       if (n === 0) {
-        n = parseFloat(k1[1]) - parseFloat(k2[1]);
+        n = parseFloat(k1[1]) - parseFloat(k2[1])
       }
-      return n;
+      return n
     })
-    .forEach(k => out.push(mapOfRows.get(k).join(" ")));
-  return unify(out);
+    .forEach(k => out.push(mapOfRows.get(k).join(' ')))
+  return unify(out)
 }
 
 // Each item in a numbered list appears as 2 rows.
@@ -69,14 +68,14 @@ function transform(mapOfRows) {
 // Attempt to fix this
 //
 function unify(rows) {
-  const rx = /^\d+[\.\)]\s*$/;
-  let out = [];
-  for (i = 0; i < rows.length; ++i) {
-    let row = rows[i];
+  const rx = /^\d+[\.\)]\s*$/
+  let out = []
+  for (let i = 0; i < rows.length; ++i) {
+    let row = rows[i]
     if (rx.test(row)) {
-      row += " " + rows[++i];
+      row += ' ' + rows[++i]
     }
-    out.push(row);
+    out.push(row)
   }
-  return out;
+  return out
 }
