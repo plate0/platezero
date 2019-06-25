c5a5ac4eded30161500335c2fc82daca9a355e27
Brian Dobby <brian@blivot.com>
2019-06-25 16:16:14 +0100

    Fix pdf line concatenation. Still not perfect.

--- frontend/server/shrimp/extractors/pdf.ts
+++ frontend/server/shrimp/extractors/pdf.ts
@@ -1,3 +1,5 @@
+//import { log } from "../common";
+
 const pdfReader = require('pdfreader')
 
 module.exports = {
@@ -5,24 +7,23 @@ module.exports = {
     let p = new Promise((resolve, reject) => {
       let rdr = new pdfReader.PdfReader({ debug: 'true' })
 
-      var mapOfRows = new Map() // indexed by [x-position, y-position]
+      var mapOfRows = new Map() // indexed by [column, x-position, y-position]
       var pageWidth
+      var debug = []
       rdr.parseFileItems(filename, function(err, item) {
+        debug.push(item)
         if (err) {
           reject(err)
         } else if (!item) {
+          //    log(debug)
           resolve(transform(mapOfRows))
         } else if (item.page && item.width) {
           pageWidth = item.width
         } else if (item.text) {
-          // accumulate text items into rows object, per line
-          const key = [calcx(item.x, pageWidth), item.y]
-          let row = mapOfRows.get(key)
-          if (!row) {
-            row = []
+          if (item.x > 0 && item.y > 0) {
+            const key = new Key(pageWidth, item.x, item.y)
+            mapOfRows.set(key, item.text)
           }
-          row.push(item.text)
-          mapOfRows.set(key, row)
         }
       })
     })
@@ -31,51 +32,69 @@ module.exports = {
   }
 }
 
-// In order to deal with 2-column pages,
-// 'x' is assumed to be 'column 0' if it
-// is less than half the current page width,
-// else 'column 1'.
-// This may be overkill and/or inadequate
-function calcx(x, pageWidth) {
-  return x >= pageWidth / 2 ? 1 : 0
-}
-
 // Transform the map of position->text into
 // an array of text rows, sorted by position
 // such that all rows in column 0 precede all rows
 // in column 1
-function transform(mapOfRows) {
+function transform(mapOfRows: Map<Key, string>): string[] {
   let out = []
   Array.from(mapOfRows.keys())
     .sort((k1, k2) => {
-      let n = parseFloat(k1[0]) - parseFloat(k2[0])
+      let n = k1.column - k2.column
       if (n === 0) {
-        n = parseFloat(k1[1]) - parseFloat(k2[1])
+        n = k1.y - k2.y
+      }
+      if (n === 0) {
+        n = k1.x - k2.x
       }
       return n
     })
-    .forEach(k => out.push(mapOfRows.get(k).join(' ')))
+    .forEach(k => out.push({ key: k, text: mapOfRows.get(k) }))
   return unify(out)
 }
 
-// Each item in a numbered list appears as 2 rows.
-// E.g.
-//      1. Line one
-// appears as
-//      1.
-//      Line one
-//
-// Attempt to fix this
-//
-function unify(rows) {
-  const rx = /^\d+[\.\)]\s*$/
+/**
+ * Each row of text may appear split into several snippets within the input;
+ * each will have the same 'y' value, but differing 'x' values.
+ * This function consolidates these snippets into one row of text.
+ *
+ * @todo Try to figure out which contiguous lines constitute a paragraph and join them together
+ *
+ * @param {KeyedText[]}   an array of {key:, text:} where key is {column:, x:, y:}
+ * @return {string[]}
+ */
+function unify(input: KeyedText[]): string[] {
   let out = []
-  for (let i = 0; i < rows.length; ++i) {
-    let row = rows[i]
-    if (rx.test(row)) {
-      row += ' ' + rows[++i]
+  for (let i = 0; i < input.length; ) {
+    let text = ''
+    for (
+      let row = input[i], y = row.key.y;
+      i < input.length && row.key.y == y;
+      row = input[++i]
+    ) {
+      text += ' ' + input[i].text
     }
-    out.push(row)
+    out.push(text)
   }
   return out
+}
+
+class Key {
+  x: number
+  y: number
+  column: number
+  constructor(pageWidth: number, x: number, y: number) {
+    this.column = x >= pageWidth / 2 ? 1 : 0
+    this.x = x
+    this.y = y
+  }
+}
+
+class KeyedText {
+  key: Key
+  text: string
+  constructor(key, text) {
+    this.key = key
+    this.text = text
+  }
 }
