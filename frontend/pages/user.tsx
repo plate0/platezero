6d9fd41262f6dfed20ea77a112dae9060ae004ba
Ben Burwell <ben@benburwell.com>
2019-07-16 10:58:08 -0400

    Favorites tab
    Add a tabbed navigation bar to the user home page letting them choose
    between seeing a searchable list of all of their recipes (the default),
    or their favorites list by navigating to /:user?tab=favorites.

--- frontend/pages/user.tsx
+++ frontend/pages/user.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as _ from 'lodash'
-import { Row, Col, Button } from 'reactstrap'
+import { Row, Col, Button, Nav, NavItem, NavLink } from 'reactstrap'
 import Head from 'next/head'
 import { withRouter, WithRouterProps } from 'next/router'
 import * as parse from 'url-parse'
@@ -11,6 +11,7 @@ import {
   Layout,
   UserSidebar,
   UserPageRecipes,
+  RecipeList,
   ProfilePicture,
   IfLoggedIn
 } from '../components'
@@ -18,9 +19,12 @@ import { UserJSON, RecipeJSON } from '../models'
 import { api } from '../common/http'
 import { getName } from '../common/model-helpers'
 
+const FAVORITES = 'favorites'
+
 interface UserProps {
   query?: string
   sort?: string
+  tab?: string
   user?: UserJSON
   recipes?: RecipeJSON[]
   statusCode?: number
@@ -33,12 +37,17 @@ class User extends React.Component<UserProps & WithRouterProps> {
 
   static async getInitialProps({ query, res }) {
     try {
-      const { username, q, sort } = query
+      const { username, q, sort, tab } = query
+      const recipes =
+        tab === FAVORITES
+          ? _.map(await api.getFavorites(username), 'recipe')
+          : await api.getRecipes({ username, q, sort })
       return {
         user: await api.getUser(username),
         query: q,
         sort,
-        recipes: await api.getRecipes({ username, q, sort })
+        tab,
+        recipes
       }
     } catch (err) {
       const statusCode = err.statusCode || 500
@@ -54,7 +63,7 @@ class User extends React.Component<UserProps & WithRouterProps> {
     if (statusCode) {
       return <ErrorPage statusCode={statusCode} />
     }
-    const { recipes, query, sort } = this.props
+    const { recipes, query, sort, tab } = this.props
     return (
       <Layout title={getName(user)}>
         <Head>
@@ -73,14 +82,29 @@ class User extends React.Component<UserProps & WithRouterProps> {
             </IfLoggedIn>
           </Col>
           <Col xs={12} sm={8} md={9} lg={10}>
-            <h2 className="m-0">Recipes</h2>
-            <UserPageRecipes
-              initialQuery={query}
-              initialSort={sort}
-              initialRecipes={recipes}
-              username={user.username}
-              baseURL={parse(this.props.router.asPath).pathname}
-            />
+            <Nav tabs>
+              <NavItem>
+                <Link to={`/${user.username}`} passHref>
+                  <NavLink active={tab !== FAVORITES}>Recipes</NavLink>
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link to={`/${user.username}?tab=favorites`} passHref>
+                  <NavLink active={tab === FAVORITES}>Favorites</NavLink>
+                </Link>
+              </NavItem>
+            </Nav>
+            {tab === FAVORITES ? (
+              <RecipeList recipes={recipes} />
+            ) : (
+              <UserPageRecipes
+                initialQuery={query}
+                initialSort={sort}
+                initialRecipes={recipes}
+                username={user.username}
+                baseURL={parse(this.props.router.asPath).pathname}
+              />
+            )}
           </Col>
         </Row>
       </Layout>
