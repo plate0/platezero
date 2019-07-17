aaca3d0fded2c4ca4a295ca22de6f09be5219b20
Ethan Mick <ethan@ethanmick.com>
2019-07-17 15:40:27 -0400

    Add facebook pixel

--- frontend/pages/pzp-a1.tsx
+++ frontend/pages/pzp-a1.tsx
@@ -2,6 +2,35 @@ import * as React from 'react'
 import { Card, Container, Col, Row } from 'reactstrap'
 import Head from 'next/head'
 
+const Facebook = () => (
+  <>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '383617562508203'); 
+fbq('track', 'PageView');
+  `
+      }}
+    />
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style={{ display: 'none' }}
+        src="https://www.facebook.com/tr?id=pixelID&ev=PageView&noscript=1"
+      />
+    </noscript>
+  </>
+)
+
 const Mailchimp = () => (
   <>
     <div id="mc_embed_signup">
@@ -195,6 +224,7 @@ export default class PZPA1 extends React.Component {
       <>
         <Head>
           <title>PlateZero Pro</title>
+          <Facebook />
         </Head>
         <Header />
       </>
