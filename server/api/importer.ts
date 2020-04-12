b1983b628f212aadfdd9e582849e880869ea6670
Ethan Mick <ethan@ethanmick.com>
2020-04-12 11:16:25 -0400

    Move Shrimp
    Move shrimp to: https://github.com/plate0/shrimp

--- server/api/importer.ts
+++ server/api/importer.ts
@@ -1,28 +1,24 @@
-import * as express from 'express'
-import { Import, ImportResult, ImportStatus } from '../shrimp/shrimp'
-import { AuthenticatedRequest } from './user'
-import { internalServerError } from '../errors'
-import { Recipe } from '../../models/recipe'
 import { S3 } from 'aws-sdk'
-import fetch from 'node-fetch'
+import * as express from 'express'
+import { size } from 'lodash'
 import * as prom from 'prom-client'
+import { parse as parseRecipe } from 'recipe-parser'
+import { generate } from 'shortid'
 import { parse } from 'url'
 import { HttpStatus } from '../../common/http-status'
-import { config } from '../config'
-import { size, toString } from 'lodash'
-import { generate } from 'shortid'
-import { parse as parseRecipe } from 'recipe-parser'
+import { Recipe } from '../../models/recipe'
+import { internalServerError } from '../errors'
+import { AuthenticatedRequest } from './user'
 const multer = require('multer')
 const multerS3 = require('multer-s3')
 const s3 = new S3()
-const { slackHook } = config
 
 const r = express.Router()
 
 const urlImportCounter = new prom.Counter({
   name: 'platezero_url_imports_total',
   help: 'Total number of URLs imported',
-  labelNames: ['hostname']
+  labelNames: ['hostname'],
 })
 r.post('/url', async function importUrl(req: AuthenticatedRequest, res) {
   const parsed = parse(req.body.url)
@@ -51,21 +47,10 @@ r.post('/url', async function importUrl(req: AuthenticatedRequest, res) {
   }
 })
 
-const fileImportCounter = new prom.Counter({
-  name: 'platezero_file_imports_total',
-  help: 'Total number of URLs imported',
-  labelNames: ['mimetype']
-})
-const fileSizeHistogram = new prom.Histogram({
-  name: 'platezero_file_import_bytes',
-  help: 'Imported file size in bytes',
-  labelNames: ['mimetype'],
-  buckets: [1000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000]
-})
 const filesPerUploadHistogram = new prom.Histogram({
   name: 'platezero_file_import_num_files',
   help: 'Number of files per upload',
-  buckets: [0, 1, 2, 4, 8, 16, 32, 64]
+  buckets: [0, 1, 2, 4, 8, 16, 32, 64],
 })
 const upload = multer({
   storage: multerS3({
@@ -73,72 +58,12 @@ const upload = multer({
     bucket: 'com-platezero-recipes',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) =>
-      cb(null, `${req.user.userId}/${generate()}-${file.originalname}`)
-  })
+      cb(null, `${req.user.userId}/${generate()}-${file.originalname}`),
+  }),
 })
 r.post('/file', upload.array('file'), async function importFile(req: any, res) {
   filesPerUploadHistogram.observe(req.files.length)
-  let recipe,
-    text,
-    httpStatus,
-    accepted: ImportResult[] = []
-  const results: ImportResult[] = []
-
-  // Do NOT use a lambda-based loop here;
-  // the lambda must be declared 'async' but
-  // the iterator will not 'await' the result
-  for (let i = 0; i < req.files.length; ++i) {
-    const file = req.files[i]
-    const mimetype = toString(file.mimetype)
-    fileImportCounter.inc({ mimetype })
-    fileSizeHistogram.observe({ mimetype }, file.size)
-    results.push(await Import(file, req.user.userId))
-  }
-
-  if (results.length === 1) {
-    const result = results[0]
-    console.log(`\x1b[97mImporter: Import result: ${result.status}\x1b[0m`)
-    switch (result.status) {
-      case ImportStatus.Incomplete:
-        recipe = result.recipe
-        text = result.text
-        httpStatus = HttpStatus.UnprocessableEntity
-        break
-      case ImportStatus.Imported:
-        recipe = result.recipe
-        httpStatus = HttpStatus.Created
-        break
-      default:
-        accepted.push(result)
-        httpStatus = HttpStatus.Accepted
-        break
-    }
-  } else {
-    accepted = results.filter(result => result.status === ImportStatus.Failed)
-    httpStatus = HttpStatus.Accepted
-  }
-
-  if (slackHook && accepted.length) {
-    notifySlack(req.user.username, accepted)
-  }
-
-  res.status(httpStatus).json({ recipe, text })
+  res.status(HttpStatus.Accepted).json({ upload: 'success' })
 })
-
-function notifySlack(user: any, results: ImportResult[]) {
-  fetch(slackHook, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      text: `${user.username} (id: ${user.userId}) uploaded ${
-        results.length
-      } recipes which could not be automatically imported. 
-Find them here: https://s3.console.aws.amazon.com/s3/buckets/com-platezero-recipes/?region=us-east-1
-\n ${results.map(r => r.file.originalname).join('\n')}`
-    })
-  })
-}
 
 export const importers = r
