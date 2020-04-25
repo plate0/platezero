7546e2a55e6bbc1da92c140abdf5e094e07afada
Ethan Mick <ethan@ethanmick.com>
2020-04-25 11:05:05 -0400

    Update password

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (username, email, password_hash, avatar_url, name) VALUES
-('em', 'em@platezero.com', '$2a$10$xtLGz6w08W2cYtk8h24Dw.gFoykXm3or5ghJQCIGREpQFsXTbZVci', null, 'Ethan Mick'),
+('em', 'em@platezero.com', '$2b$12$Wyb6WRkhscl4YSjwF7CsieY6XbOC16vMA/lm7mOqhppRM4NH5XYq6', null, 'Ethan Mick'),
 ('bb', 'bb@platezero.com', '$2a$15$eej36zpMJ88xtKGDnRYslOx4AhXbJ5BDV7CLunz3F0ICM7/xRaXqW', null, 'Ben'),
 ('bd', 'brian@virtyx.com', '$2b$12$mzouVwXwpOZVYwQOipr8T./qukqjE/Gu/IhV.LKmzLEHWL8nZ5oIC',  null,null);
