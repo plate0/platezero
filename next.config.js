0ace82e2a0058f35815c944b6c852cda94aa53ce
Ethan Mick <ethan@ethanmick.com>
2020-04-25 11:01:23 -0400

    Remove MDX
    * Remove TOS and Privacy Pages
    * Remove Next mdx configuration
    * Remove mdx package

--- next.config.js
+++ next.config.js
@@ -1,34 +1,27 @@
 const withSass = require('@zeit/next-sass')
-const withMDX = require('@zeit/next-mdx')({
-  extension: /.mdx$/,
-  options: {}
-})
 
 module.exports = (config, { defaultConfig }) => {
   const isProd = process.env.NODE_ENV === 'production'
-  return withMDX(
-    withSass({
-      env: {
-        cookie: {
-          expires: 365,
-          secure: isProd,
-          samesite: 'strict'
-        }
+  return withSass({
+    env: {
+      cookie: {
+        expires: 365,
+        secure: isProd,
+        samesite: 'strict',
       },
-      serverRuntimeConfig: {
-        api: {
-          url: process.env.API_URL
-        }
+    },
+    serverRuntimeConfig: {
+      api: {
+        url: process.env.API_URL,
       },
-      publicRuntimeConfig: {
-        api: {
-          url: process.env.API_URL
-        },
-        www: {
-          url: process.env.SITE_URL
-        }
+    },
+    publicRuntimeConfig: {
+      api: {
+        url: process.env.API_URL,
       },
-      pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
-    })
-  )
+      www: {
+        url: process.env.SITE_URL,
+      },
+    },
+  })
 }
