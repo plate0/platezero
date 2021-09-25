37ef45066081172088e2cf943e935ee61068ccba
Ethan Mick <ethan@ethanmick.com>
2021-09-25 11:11:16 -0400

    Remove env vars from config (#403)
    These will be either removed (if not needed), or re-added in `.env`
    files.

--- next.config.js
+++ next.config.js
@@ -2,26 +2,5 @@ const withSass = require('@zeit/next-sass')
 
 module.exports = (config, { defaultConfig }) => {
   const isProd = process.env.NODE_ENV === 'production'
-  return withSass({
-    env: {
-      cookie: {
-        expires: 365,
-        secure: isProd,
-        samesite: 'strict'
-      }
-    },
-    serverRuntimeConfig: {
-      api: {
-        url: process.env.API_URL
-      }
-    },
-    publicRuntimeConfig: {
-      api: {
-        url: process.env.API_URL
-      },
-      www: {
-        url: process.env.SITE_URL
-      }
-    }
-  })
+  return withSass({})
 }
