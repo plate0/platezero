9ed55e13d76ca6a1d1655809238de9b43a41d5eb
Ethan Mick <ethan@ethanmick.com>
2021-10-01 21:01:36 -0400

    Update `getClient` method for SSR
    We need to create a new client for a server side render request, so
    ensure any cookies are attached to it. Update prettier to not ignore the
    client file.

--- queries/client.ts
+++ queries/client.ts
@@ -1,14 +1,12 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { NextApiRequest } from 'next'
+import { GetServerSidePropsContext, NextApiRequest } from 'next'
 import { useCookie } from 'next-cookie'
 
+const CookieName = 'AUTH_TOKEN'
 
 export const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_API_ROOT,
-  cache: new InMemoryCache(),
-  headers: {
-
-  }
+  cache: new InMemoryCache()
 })
 
 export const useClient = () => {
@@ -18,22 +16,37 @@ export const useClient = () => {
     uri: process.env.NEXT_PUBLIC_API_ROOT,
     cache: new InMemoryCache(),
     headers: {
-      ...(
-        token ? {Authorization: `Bearer ${token}`} : {}
-      )
+      ...(token ? { Authorization: `Bearer ${token}` } : {})
     }
   })
 }
 
-export const getClient = (req: NextApiRequest) => {
-  const token =req.cookies['AUTH_TOKEN']
+export const getClient = (
+  ctxOrReq: NextApiRequest | GetServerSidePropsContext
+) => {
+  const token = getToken(ctxOrReq)
   return new ApolloClient({
     uri: process.env.NEXT_PUBLIC_API_ROOT,
     cache: new InMemoryCache(),
     headers: {
-      ...(
-        token ? {Authorization: `Bearer ${token}`} : {}
-      )
+      ...(token ? { Authorization: `Bearer ${token}` } : {})
     }
   })
+}
+
+function isApiRequest(req: any): req is NextApiRequest {
+  return !!req.cookies
+}
+
+function isServerSide(ctx: any): ctx is GetServerSidePropsContext {
+  return !!ctx.req
+}
+
+const getToken = (ctx: NextApiRequest | GetServerSidePropsContext): string => {
+  if (isApiRequest(ctx)) {
+    return ctx.cookies[CookieName]
+  } else if (isServerSide(ctx)) {
+    return ctx.req.cookies[CookieName]
+  }
+  return ''
 }
