249c7cb7cfa77d97983f89eadab7e077f167880a
Ethan Mick <ethan@ethanmick.com>
2021-10-02 09:54:37 -0400

    Refactor container layout for `main` and `div` (#419)
    This could be handled with Polymorphism, but for now we will stay away
    from that.

--- components/container.tsx
+++ components/container.tsx
@@ -1,11 +1,16 @@
 import cx, { Argument } from 'classnames'
+import React from 'react'
 
 const containerClass = 'container mx-auto'
 
 interface Props extends React.ComponentPropsWithoutRef<'div'> {}
 
-export const Container = ({ className, ...props }: Props) => (
-  <div className={cx(containerClass, className)} {...props} />
-)
+// return a regular function so it uses the name for the display name
+const container = (Element: 'div' | 'main') =>
+  function Container({ className, ...rest }: Props) {
+    return <Element className={cx(containerClass, className)} {...rest} />
+  }
 
+export const Container = container('div')
+export const Main = container('main')
 export const asContainer = (c?: Argument) => cx(c, containerClass)
